ab8816365660b08c0e19c3df66c7150888f4d36f
elderscrollsjesus <elderscrollsjesus@lostsidewalk.com>
2022-08-15 13:02:38 -0500

    Added houses, fences, water, trees, and wildflowers to level

--- level.py
+++ level.py
@@ -2,8 +2,9 @@ import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
-from sprites import Generic
-
+from sprites import Generic, Water, WildFlower, Tree
+from pytmx.util_pygame import load_pygame
+from support import import_folder
 
 class Level:
     def __init__(self):
@@ -16,6 +17,31 @@ class Level:
         self.overlay = Overlay(self.player)
 
     def setup(self):
+        tmx_data = load_pygame('data/map.tmx')
+
+        # house floor/furniture bottom
+        for layer in ['HouseFloor', 'HouseFurnitureBottom']:  # order is significant here
+            for x, y, surface in tmx_data.get_layer_by_name(layer).tiles():
+                Generic(pos=(x * TILE_SIZE, y * TILE_SIZE), surface=surface, groups=self.all_sprites,
+                        z=LAYERS['house-bottom'])
+        # house walls/furniture top
+        for layer in ['HouseWalls', 'HouseFurnitureTop']:  # order is significant here
+            for x, y, surface in tmx_data.get_layer_by_name(layer).tiles():
+                Generic(pos=(x * TILE_SIZE, y * TILE_SIZE), surface=surface, groups=self.all_sprites)
+        # fence
+        for x, y, surface in tmx_data.get_layer_by_name('Fence').tiles():
+            Generic(pos=(x * TILE_SIZE, y * TILE_SIZE), surface=surface, groups=self.all_sprites)
+        # water
+        for x, y, surface in tmx_data.get_layer_by_name('Water').tiles():
+            Water(pos=(x * TILE_SIZE, y * TILE_SIZE), frames=import_folder('graphics/water'), groups=self.all_sprites)
+        # trees
+        for obj in tmx_data.get_layer_by_name('Trees'):
+            Tree((obj.x, obj.y), obj.image, self.all_sprites, obj.name)
+        # wildflowers
+        for obj in tmx_data.get_layer_by_name('Decoration'):
+            WildFlower((obj.x, obj.y), obj.image, self.all_sprites)
+
+        # player
         self.player = Player((640, 360), self.all_sprites)  # move to __init__
 
         Generic(
