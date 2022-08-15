fc93c045e73c0259f1d4118d6349c05e5f2398f9
elderscrollsjesus <elderscrollsjesus@lostsidewalk.com>
2022-08-15 09:19:35 -0500

    Added basic player movement

--- player.py
+++ player.py
@@ -5,22 +5,36 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
-
+        # general setup
         self.image = pygame.Surface((32, 64))
         self.image.fill('green')
         self.rect = self.image.get_rect(center=pos)
+        # movement attributes
+        self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.speed = 200
 
     def input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_UP]:
-            print('up')
+            self.direction.y = -1
         elif keys[pygame.K_DOWN]:
-            print('down')
-        elif keys[pygame.K_RIGHT]:
-            print('right')
+            self.direction.y = 1
+        else:
+            self.direction.y = 0  # no vertical movement
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
         elif keys[pygame.K_LEFT]:
-            print('left')
+            self.direction.x = -1
+        else:
+            self.direction.x = 0  # no horizontal movement
+
+    def move(self, dt):
+        self.pos += self.direction * self.speed * dt
+        self.rect.center = self.pos
 
     def update(self, dt):
         self.input()
+        self.move(dt)
