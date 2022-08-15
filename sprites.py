b95212e2d56565f6f7ddc46e986876a310785caa
elderscrollsjesus <elderscrollsjesus@lostsidewalk.com>
2022-08-15 14:48:53 -0500

    Added particle effects to apple harvesting/tree death

--- sprites.py
+++ sprites.py
@@ -38,6 +38,23 @@ class WildFlower(Generic):
         self.hitbox = self.rect.copy().inflate(-20, -self.rect.height * 0.9)
 
 
+class Particle(Generic):
+    def __init__(self, pos, surface, groups, z, duration=200):
+        super().__init__(pos, surface, groups, z)
+        self.start_time = pygame.time.get_ticks()
+        self.duration = duration
+
+        mask_surface = pygame.mask.from_surface(surface)
+        new_surface = mask_surface.to_surface()
+        new_surface.set_colorkey((0, 0, 0))
+        self.image = new_surface
+
+    def update(self, dt):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.start_time > self.duration:
+            self.kill()
+
+
 class Tree(Generic):
     def __init__(self, pos, surface, groups, name):
         super().__init__(pos, surface, groups)
@@ -58,10 +75,14 @@ class Tree(Generic):
 
         if len(self.apple_sprites.sprites()) > 0:
             random_apple = choice(self.apple_sprites.sprites())
+            Particle(pos=random_apple.rect.topleft, surface=random_apple.image, groups=self.groups()[0],
+                     z=LAYERS['fruit'])
             random_apple.kill()
 
     def check_death(self):
         if self.health <= 0:
+            Particle(pos=self.rect.topleft, surface=self.image, groups=self.groups()[0], z=LAYERS['fruit'],
+                     duration=300)
             self.image = self.stump_surface
             self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
             self.hitbox = self.rect.copy().inflate((-10, -self.rect.height * 0.6))
